b6f3c312475a47fc0a8c413265a44c69025b23a2
Lucas Vasconcelos <lucasvasconcelospiranicarneiro@gmail.com>
2024-12-15 20:04:00 +0000

    corrigindo rota

--- src/controller/ProdutoController.js
+++ src/controller/ProdutoController.js
@@ -1,42 +1,44 @@
-const mongoose = require('mongoose');
-const Produto = require('../model/Produto');
+const mongoose = require("mongoose");
+const Produto = require("../model/Produto");
 
 module.exports = {
-
     /* CRUD - Produtos */
-    async showProduto(req, res){
+    async showProduto(req, res) {
         let produto = await Produto.find();
         return res.json(produto);
     },
 
-    async storeProduto(req, res){
+    async storeProduto(req, res) {
         let produto = await Produto.create(req.body);
         return res.json(produto);
     },
 
-    async destroyProduto(req,res){
+    async destroyProduto(req, res) {
         let produto = await Produto.findByIdAndDelete(req.params.id);
         return res.json(produto);
     },
 
-    async updateProduto(req,res){
-        let produto = await Produto.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    async updateProduto(req, res) {
+        let produto = await Produto.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
         return res.json(produto);
     },
 
     /* Lista Filtros */
-    async indexProduto(req, res){
-        let produto = await Produto.find(
-            {tipoProduto: req.query.tipoProduto});
+    async indexProduto(req, res) {
+        let produto = await Produto.find({
+            tipoProduto: req.query.tipoProduto,
+        });
         return res.json(produto);
     },
 
-    async indexProdutoDuplo(req, res){
+    async indexProdutoDuplo(req, res) {
         let produto = await Produto.find({
             tipoProduto: req.query.tipoProduto,
-            categoriaProduto: req.query.categoriaProduto
+            categoriaProduto: req.query.categoriaProduto,
         });
         return res.json(produto);
-    }
-
+    },
+    
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const userController = require('./controller/userController');
-const adressController = require('./controller/adressController');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userController = require("./controller/userController");
+const adressController = require("./controller/adressController");
+const produtoController = require("./controller/ProdutoController");
+const servicoController = require("./controller/ServicoController");
 
 const app = express();
 const port = 3000;
@@ -11,16 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //rotas para User
-app.post('/user', userController.store);
-app.get('/user', userController.show);
-app.put('/user/:id', userController.update);
-app.delete('/user/:id', userController.destroy);
+app.post("/user", userController.store);
+app.get("/user", userController.show);
+app.put("/user/:id", userController.update);
+app.delete("/user/:id", userController.destroy);
 
 //rotas para Adress
-app.post('/user/adress', adressController.store);
-app.get('/user/adress', adressController.show);
-app.put('/user/adress', adressController.update);
-app.delete('/user/adress/:id', adressController.destroy);
+app.post("/user/adress", adressController.store);
+app.get("/user/adress", adressController.show);
+app.put("/user/adress", adressController.update);
+app.delete("/user/adress/:id", adressController.destroy);
 
 /* ----------- Produtos -----------*/
 /* Cadastro de Produtos */
@@ -65,14 +67,17 @@ app.get("/servico/buscarservico/", servicoController.indexServico);
 /* URL = /servico/buscarservico-duplo?categoriaServico=vacinacao&?tipoServico=gato*/
 /* URL = /servico/buscarservico-duplo?categoriaServico=banho_tossa&?tipoServico=gato*/
 /* URL = /servico/buscarservico-duplo?categoriaServico=adestramento&?tipoServico=gato*/
-app.get("/servico/buscarservico-duplo", servicoController.indexServicoDuplo)
+app.get("/servico/buscarservico-duplo", servicoController.indexServicoDuplo);
 
 /* Apresentação */
 app.get("/", (req, res) => {
      res.send("Apresentacao Loja");
 });
 
+
 app.listen(port, () => {
-    mongoose.connect('mongodb+srv://Hyandra:123456oi@ifpets.aglme.mongodb.net/?retryWrites=true&w=majority&appName=IfPets');
-     console.log('Conectado ao MongoDB e servidor');
+     mongoose.connect(
+          "mongodb+srv://Hyandra:123456oi@ifpets.aglme.mongodb.net/?retryWrites=true&w=majority&appName=IfPets",
+     );
+     console.log("Conectado ao MongoDB e servidor");
 });
